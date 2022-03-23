8000dd8e945c8d4ecaa68d714755e6a8c019cd04
travis-bittel <tbittel6@gatech.edu>
2022-03-22 21:15:30 -0400

    Add XML Settings and Console Commands

--- Source/Program.cs
+++ Source/Program.cs
@@ -5,13 +5,13 @@ using System;
 using System.Threading.Tasks;
 using DSharpPlus.Net;
 using DSharpPlus.Lavalink;
+using System.Xml;
 
 namespace BrusselMusicBot
 {
     class Program
     {
-        // <Current Date>.<New Versions On That Date>
-        private const string version = "3-14-2022.0";
+        private static BotSettings settings;
 
         static void Main()
         {
@@ -20,39 +20,144 @@ namespace BrusselMusicBot
 
         static async Task MainAsync()
         {
+            settings = new BotSettings("settings.xml");
+            Console.WriteLine($"Running Settings:\n{settings}");
+
             var discord = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "OTUxODcxMjM2MzI0MDg5ODY2.YitxKw.oFEnq1t6Na_CuWxIYxXKFWo_4NI",
+                Token = settings.token,
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.AllUnprivileged
             });
+
             var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
             {
-                StringPrefixes = new[] { "!" }
+                StringPrefixes = new[] { settings.prefix }
             });
             commands.RegisterCommands<MusicCommands>();
 
             // Lavalink
             var endpoint = new ConnectionEndpoint
             {
-                Hostname = "127.0.0.1", // From your server configuration.
-                Port = 2333 // From your server configuration
+                Hostname = settings.lavalinkHostName,
+                Port = settings.lavalinkPort
             };
 
             var lavalinkConfig = new LavalinkConfiguration
             {
-                Password = "youshallnotpass", // From your server configuration.
+                Password = settings.lavalinkPassword,
                 RestEndpoint = endpoint,
                 SocketEndpoint = endpoint
             };
             var lavalink = discord.UseLavalink();
 
-            Console.WriteLine($"Running BrusselBot Version {version}");
+            Console.WriteLine($"Running BrusselBot Version {settings.version}");
             await discord.ConnectAsync();
             Console.WriteLine($"Logged In");
             await lavalink.ConnectAsync(lavalinkConfig);
-            Console.WriteLine($"Lavalink Established");
-            await Task.Delay(-1);
+            Console.WriteLine($"Lavalink Connection Established");
+
+            // Handle console commands
+            while (true)
+            {
+                await GetInputAsync();
+            }
+        }
+
+        /// <summary>
+        /// We call this repeatedly to handle console commands.
+        /// </summary>
+        /// <returns></returns>
+        private static async Task GetInputAsync()
+        {
+            string input = Task.Run(() => Console.ReadLine()).Result;
+            switch (input)
+            {
+                case "help":
+                    Console.WriteLine(
+                        "----- Commands -----" +
+                        "\nsettings: Displays the currently used bot settings, loaded at startup" +
+                        "\nversion: Displays the currently running bot version" + 
+                        "\n--------------------");
+                    break;
+                case "settings":
+                    Console.WriteLine(
+                        $"----- Settings -----" + 
+                        $"\n{settings}" +
+                        "\n--------------------");
+                    break;
+                case "version":
+                    Console.WriteLine($"Current Version: {settings.version}");
+                    break;
+                default:
+                    Console.WriteLine("Command unknown. Type \"help\" for a list of commands.");
+                    break;
+            }
+        }
+    }
+
+    public class BotSettings
+    {
+        public string version;
+        public string token;
+        public string prefix;
+        public string lavalinkHostName;
+        public int lavalinkPort;
+        public string lavalinkPassword;
+
+        /// <summary>
+        /// Creates a new BotSettings data class from the passed in XML filepath.
+        /// Writes warnings to the console if the file is not loaded properly or
+        /// any settings are missing.
+        /// </summary>
+        /// <param name="filepath"></param>
+        public BotSettings(string filepath)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filepath);
+                XmlElement root = doc.DocumentElement;
+                Console.WriteLine(root.Name);
+
+                version = root.SelectSingleNode("version").InnerText;
+                token = root.SelectSingleNode("token").InnerText;
+                prefix = root.SelectSingleNode("prefix").InnerText;
+                lavalinkHostName = root.SelectSingleNode("lavalink/hostname").InnerText;
+                lavalinkPort = int.Parse(root.SelectSingleNode("lavalink/port").InnerText);
+                lavalinkPassword = root.SelectSingleNode("lavalink/password").InnerText;
+            }
+            catch
+            {
+                Console.WriteLine("Failed to load settings from settings.xml! " +
+                    "Make sure settings.xml is properly formatted and in the " +
+                    "same directory as the exe file!");
+            }
+
+            // Warn the user if any settings are missing
+            if (string.IsNullOrEmpty(token.Trim()))
+            {
+                Console.WriteLine("[ERROR]: No bot token specified");
+            }
+            if (string.IsNullOrEmpty(prefix.Trim()))
+            {
+                Console.WriteLine("[ERROR]: No command prefix specified");
+            }
+            if (string.IsNullOrEmpty(lavalinkHostName.Trim()))
+            {
+                Console.WriteLine("[ERROR]: No Lavalink Host Name specified");
+            }
+            if (string.IsNullOrEmpty(lavalinkPassword.Trim()))
+            {
+                Console.WriteLine("[ERROR]: No Lavalink Password specified");
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"Version: {version}\nToken: {token}\nPrefix: {prefix}\nLL Host: {lavalinkHostName}" +
+                $"\nLL Port: {lavalinkPort}\nLL Password: {lavalinkPassword}";
         }
     }
 }
